026987c1ce08c60f9b54872467f45faa1932eab0
Feike Geerts <feike.geerts@storiogroup.com>
2025-04-09 09:27:22 +0200

    Fix broken import

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -13,7 +13,7 @@ const config = {
 			strict: true
 		}),
 		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/career-timeline' : ''
+			base: process.env.NODE_ENV === 'production' ? '' : ''
 		}
 	}
 };
